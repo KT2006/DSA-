204debf8c6a24ee0773688c9e771a0edcbf9d290
Kshitij Totawar <kshitij.totawar@gmail.com>
2024-09-24 18:32:40 +0530

    Add files via upload
    I added solution for linear search in linked list

---
+++ Linked List/searchLLByGpt.cpp
@@ -0,0 +1,15 @@
+// void search(Node* &head, int target) {
+//     Node* temp = head;
+//     int index = 0;
+    
+//     while(temp != NULL) {
+//         if(temp->data == target) {
+//             cout << "The target is present in the LL at index: " << index << endl;
+//             return; // Exit after finding the target
+//         }
+//         temp = temp->next;
+//         index++;
+//     }
+    
+//     cout << "The target is not present in the LL." << endl;
+// }
